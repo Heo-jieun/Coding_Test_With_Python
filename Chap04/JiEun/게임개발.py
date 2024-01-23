66f10e97ac719f23eaecc728d201d4d45566f128
Heo-jieun <jieunheo192@gmail.com>
2024-01-23 21:31:16 +0900

    Update file

--- Chap04/JiEun/게임개발.py
+++ Chap04/JiEun/게임개발.py
@@ -19,6 +19,9 @@ around = 0
 
 # 움직임
 while 1 :
+    # 왼쪽으로 돌기
+    d = direction[d]
+
     a = A + dx[d]
     b = B + dy[d]
     if(0 <= a < N) and (0 <= b < M ) :
@@ -29,12 +32,10 @@ while 1 :
             B = b
             maps[A][B] = 'X'
             cnt += 1
-            d = direction[d]
             around = 0
         # 갈 수 없는 길인 경우
         elif (maps[a][b] == 1) or (maps[a][b] == 'X'):
             print('block')
-            d = direction[d]
             around += 1
     else : 
         break
